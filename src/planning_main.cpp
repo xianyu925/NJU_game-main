3d36c3dbf636bc33161732575c84d444784bff9b
xianyu <2860729217@qq.com>
2025-06-24 22:11:52 +0800

    version0.0.2

--- src/planning_main.cpp
+++ src/planning_main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include "process.h"
-
+#include "show_result.h"
 using std::cout, std::endl;
 
 int main() {
@@ -9,6 +9,10 @@ int main() {
   Process pro;
   pro.planProcess();
   cout << "planing end" << endl;
+
+  cout << "show result: " << endl;
+  ShowResult show;
+  show.drawResult();
 
   return 0;
 }
